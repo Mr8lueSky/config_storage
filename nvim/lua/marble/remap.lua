f9f8f33d232687e6ffa59cecd483ffb54618cc63
Saveli Khulup <savahulup@gmail.com>
2025-06-27 16:57:52 +0300

    Changes to nvim config

--- nvim/lua/marble/remap.lua
+++ nvim/lua/marble/remap.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-q>", function()
-	vim.cmd(":q")
-end)
+vim.keymap.set("n", "<C-q>", function() vim.cmd(":q") end)
 vim.keymap.set("n", "<leader>dd", '"_dd')
 
 -- ctrl + backspace in insert mode to delete whole words
@@ -15,7 +13,7 @@ vim.cmd("nnoremap <M-j> <C-w>j")
 vim.cmd("nnoremap <M-k> <C-w>k")
 vim.cmd("nnoremap <M-l> <C-w>l")
 
--- jk to enter visual mode from insert
+-- jk to enter normal mode from insert
 vim.keymap.set('i', 'jk', '<Esc>', {noremap = true})
 
 -- ctrl + k and ctrl + j to page up/down
@@ -24,3 +22,16 @@ vim.cmd("nnoremap <C-k> <PageUp>")
 
 -- paste without yanking
 vim.cmd('vnoremap <leader>o "_dp')
+
+-- Exit terminal on ctrl + x
+vim.cmd(":tnoremap <C-x> <C-\\><C-n>")
+
+
+vim.keymap.set("n", "<leader>th", function ()
+	local win_id = vim.api.nvim_get_current_win();
+	vim.cmd(":split")
+	vim.api.nvim_set_current_win(win_id)
+	vim.cmd(":terminal")
+	vim.api.nvim_win_set_height(win_id, 15)
+end)
+
