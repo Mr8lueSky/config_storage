8c361cfee6c2a0ef8756c8ed3b5ea07520b99e14
Saveli Khulup <savahulup@gmail.com>
2025-07-01 13:56:53 +0300

    Changes to nvim config

--- nvim/lua/marble/remap.lua
+++ nvim/lua/marble/remap.lua
@@ -26,6 +26,7 @@ vim.cmd('vnoremap <leader>o "_dp')
 -- Exit terminal on ctrl + x
 vim.cmd(":tnoremap <C-x> <C-\\><C-n>")
 
+-- Open horizontal terminal on space + t + h
 vim.keymap.set("n", "<leader>th", function()
     local win_id = vim.api.nvim_get_current_win();
     vim.cmd(":split")
@@ -34,6 +35,7 @@ vim.keymap.set("n", "<leader>th", function()
     vim.api.nvim_win_set_height(win_id, 15)
 end)
 
+-- Execute cmd in vertical terminal on space + t + e
 vim.keymap.set("n", "<leader>te", function()
     local win_id = vim.api.nvim_get_current_win();
     local cmd = vim.fn.input("Cmd: ", "", "shellcmd");
@@ -42,4 +44,18 @@ vim.keymap.set("n", "<leader>te", function()
     vim.api.nvim_set_current_win(win_id)
     vim.cmd(cmd_vim)
 end)
+
+-- Resize windows on ctrl + alt + hkjl
+vim.keymap.set("n", "<C-M-J>", function() vim.cmd(":resize -5") end)
+vim.keymap.set("n", "<C-M-K>", function() vim.cmd(":resize +5") end)
+vim.keymap.set("n", "<C-M-L>", function() vim.cmd(":vertical resize +5") end)
+vim.keymap.set("n", "<C-M-H>", function() vim.cmd(":vertical resize -5") end)
+
+-- Open diagnostic message on [] - o
+local open_diagnostic = function ()
+	vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
+end
+vim.keymap.set("n", "[o", open_diagnostic)
+vim.keymap.set("n", "]o", open_diagnostic)
 
