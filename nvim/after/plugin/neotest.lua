647644348cae7614fec5604b57b7fce4861accab
Saveli Khulup <savahulup@gmail.com>
2025-08-12 20:27:06 +0300

    Changes to nvim config

---
+++ nvim/after/plugin/neotest.lua
@@ -0,0 +1,11 @@
+require("neotest").setup({
+    adapters = {
+        require("neotest-python")({
+            dap = {justMyCode = false},
+            args = {"--log-level", "DEBUG"},
+            runner = "pytest"
+        })
+    }
+})
+
+vim.keymap.set("n", "<leader>ts", ":Neotest summary\r\n")
