ba6f2e8b7bd95172318e55238f2dcd8a5f2398e2
Saveli Khulup <savahulup@gmail.com>
2025-09-05 10:59:17 +0300

    Changes to nvim config

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
         ensure_installed = {"lua_ls", "basedpyright", "ts_ls"},
-        automatic_enable = {exclude = {"basedpyright"}}
+        automatic_enable = {exclude = {"basedpyright", "jedi-language-server"}}
     },
     dependencies = {
         {"mason-org/mason.nvim", opts = {}}, "neovim/nvim-lspconfig"
