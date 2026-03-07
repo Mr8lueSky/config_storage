8dec7e4a9f7f54c2b079fbc2bc0fc5f6a052aeb1
Saveli <savahulup@gmail.com>
2026-03-07 23:51:21 +0300

    Changes to nvim config

--- nvim/lua/marble/init.lua
+++ nvim/lua/marble/init.lua
@@ -2,6 +2,7 @@ require("marble.autocomplete")
 require("marble.remap")
 
 vim.cmd("set rnu")
+vim.cmd("set number")
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 -- allow netrw to copy files to same directory 
 vim.cmd [[
