57b523061ffdb038a1cefb0220ff5ef3b18d5dc3
skhulup <skhulup@ozon.ru>
2026-07-12 14:50:18 +0300

    Changes to nvim config

--- nvim/lua/marble/init.lua
+++ nvim/lua/marble/init.lua
@@ -27,3 +27,5 @@ end, { desc = "Toggle diagnostic virtual text" })
 vim.keymap.set("n", "<leader>ih", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end)
+
+vim.keymap.set("n", ":", "q:i", { desc = "Open command-line window" })
